3f92cdf4772c97cb9269776e13855e3c02286ca0
Risky Ade Sucahyo <riskyadesucahyo@gmail.com>
2022-12-21 11:42:47 +0700

    update detil project

--- resources/views/detilProject.blade.php
+++ resources/views/detilProject.blade.php
@@ -8,30 +8,39 @@
             <h2 class="mb-3">{{ $project->title }}</h2>
             
             <div style="max-height: 350px; overflow:hidden;">
-                <img src="cover/{{ $project->cover }}"
-                alt="" class="img-fluid">
+                {{-- <img src="cover/{{ $project->cover }}"
+                alt="" class="img-fluid"> --}}
+                @if($project->cover)
+                <img src="/cover/{{ $project->cover }}" class="img-fluid"alt="">
+              @else
+                <img src="/img/blank_img.png" class="img-fluid" alt="#">
+              @endif
             </div>
-          
-            {{-- @if($post->image)
-            <div style="max-height: 350px; overflow:hidden;">
-                <img src="{{ asset('storage/' . $post->image) }}"
-                alt="{{ $post->category->name }}" class="img-fluid">
-            </div>
-            @else
-                <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}"
-                alt="{{ $post->category->name }}" class="img-fluid ">
-            @endif --}}
 
-            {{-- <article class="my-3 fs-5">
+            
+            
+        </div>
+        <div class="col-md-8 fs-5">
 
-            {!! $post->body !!}
-
-            </article> --}}
-
+        </div>
+        <div class="col-md-6 fs-6">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><i class="bi bi-check-all colours"></i>
+                  Nama Client : {{ $project->client->name }}
+                </li>
+                <li class="list-group-item"><i class="bi bi-check-all colours"></i>
+                  Scope : {{ $project->service->title }}
+                </li>
+                <li class="list-group-item"><i class="bi bi-check-all colours"></i>
+                  Alamat Proyek : {{ $project->alamat }}
+                </li>
+                <li class="list-group-item"><i class="bi bi-check-all colours"></i>
+                  Tanggal Selesai Proyek : {{ $project->tanggal }}
+                </li>
+              </ul>
+        </div>
+      
         <a href="/project" class="d-block mt-3">Back to all project</a>
-
-
-    </div>
 </div>
 
 @endsection
